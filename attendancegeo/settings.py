5b2610188577517d65abfae460c1551ce8add917
devisknown <starblegamer@gmail.com>
2026-07-29 14:11:54 +0000

    added 404 page incase of errors

--- attendancegeo/settings.py
+++ attendancegeo/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2yaw)c5=vf(kggfv*^=9=m7925a$j$99c4&3f65)ss9*ke9tqa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
 
 # Application definition
